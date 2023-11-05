b082772d7db57ee10f22d5452ed58ca1f6109d73
PatriceAlan <peckpatricealan@gmail.com>
2023-11-05 11:29:59 +0100

    Database changes

--- biblioverse/biblioverse/settings.py
+++ biblioverse/biblioverse/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'biblioverse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
