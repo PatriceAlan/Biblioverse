c735330e3da977ec0e51cf2fe2a493906b652e03
PatriceAlan <peckpatricealan@gmail.com>
2023-11-08 23:06:38 +0100

    models

--- biblioverse/boeken/models.py
+++ biblioverse/boeken/models.py
@@ -24,3 +24,12 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.user.username} for {self.ebook.title}"
+
+class Bookmark(models.Model):
+    page = models.PositiveBigIntegerField()
+    bookmark_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    ebook = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bookmark by {self.user.username} for {self.ebook.title} at page {self.page}"
