2926e7c83110c74357ac9b400ebda52cffbf7fe9
PatriceAlan <peckpatricealan@gmail.com>
2023-11-13 14:11:00 +0100

    categories MVC completed

--- biblioverse/boeken/urls.py
+++ biblioverse/boeken/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,7 @@ urlpatterns = [
 
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
