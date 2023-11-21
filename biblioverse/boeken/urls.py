e8a1d840cc0611bd49a6b207cda0806d9412f854
PatriceAlan <peckpatricealan@gmail.com>
2023-11-21 23:20:47 +0100

    ...

--- biblioverse/boeken/urls.py
+++ biblioverse/boeken/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('author/<int:author_id>/books/', views.author_books, name='author_books'),
     path('category/<int:category_id>/books/', views.category_books, name='category_books'),
 
-    path('logout/', views.logout_user, name='logout'),
+    path('logout/', views.logout_user, name='logout'), 
     path('register/', views.register_user, name='register'),
     
 ]
